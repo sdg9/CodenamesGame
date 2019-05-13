5ba87667a15f1ab8024ead8911aecbd8b2ebb2a0
Steven Goff <steveng9@gmail.com>
2019-05-13 15:22:26 -0500

    Starting conversion to messaging with binary

--- server/test/MessagePackTest.kt
+++ server/test/MessagePackTest.kt
@@ -5,6 +5,10 @@ import com.daveanthonythomas.moshipack.MoshiPack
 import com.example.common.*
 import okio.BufferedSource
 import okio.ByteString
+import org.msgpack.core.MessagePack
+import org.msgpack.value.ValueType
+import java.io.ByteArrayOutputStream
+import java.util.*
 import kotlin.test.*
 
 
@@ -24,18 +28,6 @@ class MessagePackTest {
 //        println("Total bytes: ${packed.}")
         println(packed.readByteString().hex())
 
-//
-//        val originalMessage = UserConnected("1234","User1")
-//        val json = toJSON(originalMessage)
-//
-//        assertEquals(
-//            "{\"id\":\"1234\",\"name\":\"User1\",\"type\":\"USER_CONNECTED\"}", json
-//        )
-//
-//        val parsedMessage = fromJson<UserConnected>(json)
-//        assertEquals(
-//            originalMessage, parsedMessage
-//        )
     }
 
     @Test
@@ -48,4 +40,70 @@ class MessagePackTest {
 
         println(plug)
     }
+
+    @Test
+    fun packVariousWays() {
+
+        val bytes = MoshiPack().packToByteArray(MessagePackWebsitePlug())
+        val bytes2 = ByteString.decodeHex("82a7636f6d70616374c3a6736368656d6100").toByteArray()
+        val bytes3 = MoshiPack().packToByteArray(MessagePackWebsitePlug())
+
+        assertNotEquals(bytes, bytes3)
+
+        val unpacked1: MessagePackWebsitePlug = MoshiPack().unpack(bytes)
+//        println(unpacked1)
+
+        val unpacked2: MessagePackWebsitePlug = MoshiPack().unpack(bytes2)
+//        val unpacked2: MessagePackWebsitePlug = MoshiPack().unpack(bytes2)
+//        println(unpacked2)
+        val unpacked3: MessagePackWebsitePlug = MoshiPack().unpack(bytes3)
+//        println(unpacked3)
+
+        assertEquals(unpacked1, unpacked2)
+        assertEquals(unpacked1, unpacked3)
+    }
+
+    @Test
+    fun whyDoByteArraysNotEqual() {
+        // TODO understand why this is the case where arrays don't equal
+        val someHexString = "82a7636f6d70616374c3a6736368656d6100"
+        val bytes1 = ByteString.decodeHex(someHexString).toByteArray()
+        val bytes2 = ByteString.decodeHex(someHexString).toByteArray()
+
+        // Not sure why but byte arrays are not equal or same despite
+        assertNotSame(bytes1, bytes2)
+        assertNotEquals(bytes1, bytes2)
+    }
+
+
+    @Test
+    fun byteArrayManipulation() {
+        val someHexString = "82a7636f6d70616374c3a6736368656d6100"
+        val bytes = ByteString.decodeHex(someHexString).toByteArray()
+        val unpackedObject1: MessagePackWebsitePlug = MoshiPack().unpack(bytes)
+
+        val packedByteArray = packProtocol(Protocol.USER_ID, bytes)
+
+        // send over socket
+
+        val unpacedBytes = unpackProtocol(packedByteArray)
+        assertEquals(unpacedBytes.protocol, Protocol.USER_ID)
+        val unpackedObject2: MessagePackWebsitePlug = MoshiPack().unpack(unpacedBytes.byteArray)
+        assertEquals(unpackedObject1, unpackedObject2)
+    }
+}
+
+fun packProtocol(protocol: Int, bytes: ByteArray): ByteArray {
+    val outputStream = ByteArrayOutputStream()
+    outputStream.write(protocol)
+    outputStream.write(bytes)
+    return outputStream.toByteArray()
+}
+
+data class DecryptProtocol(val protocol: Int, val byteArray: ByteArray)
+
+fun unpackProtocol(bytes: ByteArray): DecryptProtocol {
+    val protocol = bytes[0].toInt()
+    val newByteArray = Arrays.copyOfRange(bytes, 1, bytes.size)
+    return DecryptProtocol(protocol, newByteArray)
 }
