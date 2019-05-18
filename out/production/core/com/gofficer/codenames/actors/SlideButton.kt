546cc241103cf6adc33ffa92f26f42c3b8779a4d
Steven Goff <steveng9@gmail.com>
2019-05-18 16:43:40 -0500

    Deleted out dirs

--- out/production/core/com/gofficer/codenames/actors/SlideButton.kt
+++
@@ -1,7 +0,0 @@
-package com.gofficer.codenames.actors
-
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
-
-
-class SlideButton(text: String, skin: Skin, style: String, val id: Int) : TextButton(text, skin, style)
--- out/production/core/com/gofficer/codenames/game/MainActions.kt
+++
@@ -1,5 +0,0 @@
-package com.gofficer.codenames.game
-
-import com.gofficer.codenames.redux.Action
-
-data class ChangeColor(val red: Float, val green: Float, val blue: Float): Action
